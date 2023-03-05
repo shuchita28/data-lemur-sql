633ee380f159f618f2570bd06b587d8c13c62d9f
Shuchita Mishra <71638384+shuchita28@users.noreply.github.com>
2023-03-05 11:26:52 -0500

    Solution for problem

---
+++ Cities With Completed Trades.sql
@@ -0,0 +1,6 @@
+SELECT u.city, COUNT(t.user_id) AS total_orders
+FROM trades t JOIN users u ON t.user_id = u.user_id
+WHERE t.status = 'Completed'
+GROUP BY u.city
+ORDER BY total_orders DESC
+LIMIT 3;
