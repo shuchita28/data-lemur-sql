876c34aac106d6a5080d16c6fba741a80aff0522
Shuchita Mishra <71638384+shuchita28@users.noreply.github.com>
2023-03-11 18:50:46 -0500

    Solution for problem

---
+++ Active User Retention.sql
@@ -0,0 +1,18 @@
+--SELECT COUNT(DISTINCT(user_id))
+--FROM user_actions
+--WHERE user_id IN (SELECT user_id 
+--      FROM user_actions
+--      WHERE DATE_PART('month', event_date) IN (7))
+--  AND DATE_PART('month', event_date) IN (6)
+
+SELECT 
+  EXTRACT(MONTH FROM curr_month.event_date) AS month,
+  COUNT(DISTINCT(curr_month.user_id)) AS monthly_active_users
+FROM user_actions AS curr_month
+WHERE EXISTS (
+  SELECT last_month.user_id
+  FROM user_actions AS last_month
+  WHERE last_month.user_id = curr_month.user_id
+  AND EXTRACT(MONTH FROM last_month.event_date) = EXTRACT(MONTH FROM curr_month.event_date - interval '1 month')
+) AND EXTRACT(MONTH FROM curr_month.event_date) = 7
+GROUP BY EXTRACT(MONTH FROM curr_month.event_date);
